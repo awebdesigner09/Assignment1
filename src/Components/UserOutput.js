36cf76a92c5a8f1a616f3a802cb0ec635a5b96e3
Naga Sai <awebdesigner09@gmail.com>
2020-10-21 05:32:43 +0530

    Add files via upload

--- src/Components/UserOutput.js
+++ src/Components/UserOutput.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './UserOutput.css'
 const userOutput = (props) => {
     return(
-    <div>
+    <div className="output">
         <p>
             {props.name}
         </p>
